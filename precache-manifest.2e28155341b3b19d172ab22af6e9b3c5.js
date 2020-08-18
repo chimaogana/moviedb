dc947ad87c44fb6c02a3dbf3ad77d7da6f3695dc
clevadani <clevadani@gmail.com>
2020-08-18 01:27:32 +0100

    Updates

---
+++ precache-manifest.2e28155341b3b19d172ab22af6e9b3c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14dccf9be43b5365c617a3463ea5cb49",
+    "url": "/moviedb/index.html"
+  },
+  {
+    "revision": "391603772dc01a2a4cdb",
+    "url": "/moviedb/static/css/main.0a8983cc.chunk.css"
+  },
+  {
+    "revision": "99d0ffb60db12e2458df",
+    "url": "/moviedb/static/js/2.9deade40.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moviedb/static/js/2.9deade40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "391603772dc01a2a4cdb",
+    "url": "/moviedb/static/js/main.2fb79898.chunk.js"
+  },
+  {
+    "revision": "dff7d7152d1d72c996c0",
+    "url": "/moviedb/static/js/runtime-main.c7335139.js"
+  }
+]);
